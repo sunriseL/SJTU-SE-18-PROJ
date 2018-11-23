aa44c44661a13a2a491c510e4b87e93fb958f4ca
sunriseL <540729251@qq.com>
2018-11-23 18:50:31 +0800

    complete part b

---
+++ hw3/Part B/producer.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import json
+from kafka import KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+msg_dict = {
+    "sleep_time": 1,
+
+    "table": "msg",
+    "msg": "Hello World"
+}
+msg = json.dumps(msg_dict).encode('utf-8')
+for i in range(10):
+    producer.send('test', msg, partition=0)
+producer.close()
